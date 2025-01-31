4f98529e89e4626ff201b69e36e31326fcc1be8d
tvallois <theo.vallois@hotmail.fr>
2025-02-01 00:10:20 +0100

    updated game logic, menus, key listening

--- src/utils/inputHandler.js
+++ src/utils/inputHandler.js
@@ -1,9 +1,31 @@
 //Class to capture/track player keypress events in realTime
 export default class InputHandler {
     constructor() {
-        this.keys = {}; //object to store current state of pressed keys
-        window.addEventListener("keydown", (e) => (this.keys[e.key] = true)); //set key pressed to true
-        window.addEventListener("keyup", (e) => (this.keys[e.key] = false)); //set key pressed to false 
+        this.keys = {}; // pressed keys
+        this.justPressedKeys = new Set(); // keys just pressed 
+
+        window.addEventListener("keydown", (e) => {
+            if (!this.keys[e.key]) {
+                this.justPressedKeys.add(e.key);
+            }
+            this.keys[e.key] = true; 
+        });
+        window.addEventListener("keyup", (e) => {
+            this.keys[e.key] = false;
+            this.justPressedKeys.delete(e.key); 
+        }); 
+    }
+
+    isKeyPressed(key) {
+        return this.keys[key];
+    }
+
+    isKeyJustPressed(key) {
+       if (this.justPressedKeys.has(key)) {
+        this.justPressedKeys.delete(key);
+        return true;
+       }
+       return false;
     }
 }
 
@@ -24,4 +46,6 @@ EventListeners : window can lsiten for events at a global lvl :
 /*
 using default when declaring a class allows to import the class directly in another file whithout {}
 in this exemple : import InputHandler from '../utils/inputHandler.js';
+it works only when you need to export only ONE element
+when you need to export multiple elements still neeed to use the basic export option and the {} to import them
 */

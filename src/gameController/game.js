586084dbfde929f489b1229272a63024533344a1
tvallois <theo.vallois@hotmail.fr>
2025-02-01 00:06:19 +0100

    updated game logic, menus, key listening

--- src/gameController/game.js
+++ src/gameController/game.js
@@ -1,70 +1,110 @@
-import UIManager from './UIManager.js';
+import {createMenu, createPauseMenu, createGameOverScreen} from './menu.js';
 import StateManager from './stateManager.js';
 import { Player } from '../objects/player.js';
 import { Enemy } from '../objects/enemy.js';
 import InputHandler from '../utils/inputHandler.js';
+import FPSManager from './FPSManager.js';
 
 export default class Game {
     constructor() {
-        this.gameContainer = document.getElementById('game-container');
-        this.uiManager = new UIManager(this);
         this.stateManager = new StateManager();
+        this.running = false;
+        this.score = 0;
+
+        this.gameContainer = document.getElementById('game-container');
+        this.fpsManager = new FPSManager(60, 80);
+        this.inputHandler = new InputHandler();
 
         this.player = null;
         this.enemy = null;
-        this.inputHandler = new InputHandler();
 
-        this.init();
+        this.menuScreen = createMenu(() => this.startGame());
+        this.pauseMenu = createPauseMenu(
+            () => this.resumeGame(),
+            () => this.restartGame()
+        );
+        this.gameOverScreen = createGameOverScreen(
+            () => this.restartGame(),
+            () => window.location.reload()
+        );
     }
 
-    init() {
-        this.uiManager.createMainMenu();
-    }
-
-    startGame() {
-        this.uiManager.hideMainMenu();
-        this.stateManager.setRunning();
-
-        this.player = new Player(this.gameContainer);
-        this.enemy = new Enemy(this.gameContainer);
-
-        this.gameLoop();
-    }
-
-    gameLoop(){
-        if (!this.stateManager.isRunning()) return;
-
-        this.update();
-        this.render();
-
-        requestAnimationFrame(() => this.gameLoop());
-    }
-
-    udpate() {
-        this.player.update(this.inputHandler.keys);
-        this.enemy.update();
-
-        if (this.player.isDestroyed()) {
-            this.uiManager.showGameOverScreen(this.stateManager.score);
-            this.stateManager.SetGameOver();
+    //game logic for key presse
+    handleKeyPress() {
+        if (this.inputHandler.isKeyJustPressed("Escape")) {
+            if (this.stateManager.isRunning()) {
+                this.pauseGame();
+            } else if (this.stateManager.isPaused()){
+                this.resumeGame();
+            }
         }
     }
 
-    render() {
+    restartGame() {
+        this.pauseMenu.classList.add("hidden");
+        this.gameOverScreen.classList.add("hidden");
+        this.stateManager.resetGame();
+        this.startGame();
+    }
+
+    startGame() {
+        this.menuScreen.classList.add("hidden");
+        this.stateManager.setRunning();
+        this.running = true;
+        this.score = 0;
+
+        //player/enemies initialization
+
+        this.gameLoop(performance.now());
+    }
+
+    gameLoop(timestamp) {
+        const dt = this.fpsManager.update(timestamp);
+
+        if (this.running && dt > 0) {
+            /*
+            this.updateGame();
+            this.renderGame();
+            */
+        }
+
+        this.handleKeyPress();
+        requestAnimationFrame((ts) => this.gameLoop(ts));
+    }
+
+    /* // player/enemies movement mechanics
+    udpateGame() {
+        this.player.update(this.inputHandler.keys);
+        this.enemy.update();
+
+        //collision logic
+    }
+
+    renderGame() {
         this.player.render();
         thie.enemy.render();
     }
-
+ */
     pauseGame() {
+        this.running = false;
         this.stateManager.setPaused();
-        this.uiManager.showPauseMenu();
+        this.pauseMenu.classList.remove("hidden");
     }
 
     resumeGame() {
+        this.running = true;
         this.stateManager.setRunning();
-        this.uiManager.hidePauseMenu();
-        this.gameLoop();
+        this.pauseMenu.classList.add("hidden");
+
+        //continue game loop
+        this.gameLoop(performance.now());
+    }
+
+    gameOver() {
+        this.running = false;
+        this.stateManager.setGameOver();
+        this.gameOverScreen.classList.remove("hidden");
+        document.getElementById("finalScore").textContent =`Score: ${this.score}`;
     }
 }
 
-new Game();
